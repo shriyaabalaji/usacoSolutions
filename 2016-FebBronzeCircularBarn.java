58c0060ff170da0eabe5562e71718f14ed15d0e2
Shriyaa Balaji <shriyaabalaji007@gmail.com>
2024-04-01 13:00:31 -0500

    Update 2016-FebBronzeCircularBarn.java

--- 2016-FebBronzeCircularBarn.java
+++ 2016-FebBronzeCircularBarn.java
@@ -1,1 +1,44 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
+class Main {
+  public static void main(String[] args) throws FileNotFoundException{
+    Scanner input = new Scanner(new File("cbarn.in"));
+    PrintWriter output = new PrintWriter("cbarn.out");
+    int numberOfRooms = input.nextInt();
+    int minDist = 0;
+    ArrayList<Integer> cows = new ArrayList<Integer>();
+    
+    // Add cow numbers to ArrayList
+    for(int i = 0; i < numberOfRooms; i++){
+      cows.add(input.nextInt());
+    }
+
+    for(int i = 0; i < cows.size(); i++){
+      int temp = cows.get(0);
+      cows.remove(0);
+      cows.add(temp);
+      int distance = 0;
+      if(i == 0){
+        for(int j = 1; j < cows.size(); j++){
+        distance += cows.get(j) * j;
+        }
+        minDist = distance;
+      }
+      else{
+        for(int j = 1; j < cows.size(); j++){
+          distance += cows.get(j) * j;
+        }
+        if(distance < minDist) {
+          minDist = distance;
+        }
+      }
+    }
+    output.println(minDist);
+    input.close();
+    output.close();
+  }
+}
